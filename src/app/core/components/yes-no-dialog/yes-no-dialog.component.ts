afc60984cfff25720fec58ef9930d07a2c2882e4
FurukawaTetsuji <tetsufuru1968+1@gmail.com>
2021-09-08 02:56:05 +0900

    ✨Changed from TSLint to ESLint.

--- src/app/core/components/yes-no-dialog/yes-no-dialog.component.ts
+++ src/app/core/components/yes-no-dialog/yes-no-dialog.component.ts
@@ -9,8 +9,6 @@ import { YesNoDialogData } from '../../models/yes-no-dialog-data';
   templateUrl: './yes-no-dialog.component.html',
   styleUrls: ['./yes-no-dialog.component.scss']
 })
-export class YesNoDialogComponent implements OnInit {
+export class YesNoDialogComponent {
   constructor(public translateService: TranslateService, @Inject(MAT_DIALOG_DATA) public data: YesNoDialogData) {}
-
-  ngOnInit(): void {}
 }
